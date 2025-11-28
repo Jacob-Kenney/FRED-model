c8ac033c1733d44426568268beef66bd70579991
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-11-28 07:50:44 +0000

    feat: add LSTM model architecture

--- LSTM.py
+++ LSTM.py
@@ -6,7 +6,15 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 class Model(nn.Module):
-    ...
+    def __init__(self, Np: int, Nf: int, hidden_dim: int = 128, num_layers: int = 2, dropout: float = 0.1):
+        super().__init__()
+        self.Np = Np
+        self.Nf = Nf
+
+        # LSTM layer
+        self.lstm = nn.LSTM(input_size = 4, hidden_size = hidden_dim, num_layers = num_layers, dropout = dropout)
+        # Prediction head [Nf, 4]
+        self.classifier = nn.Linear(hidden_dim, Nf * 4)
 
 # Consume processing function, batch size, produce tuple of train and test dataloaders (train, test)
 def get_dataloaders(dataset_name: str, process_function: None, batch_size: int = None, shuffle: bool = False) -> tuple[DataLoader, DataLoader]:
