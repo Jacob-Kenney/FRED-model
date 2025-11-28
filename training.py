b3fefd03732fec7ba141d514fc7b0845dedb86fa
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-11-28 10:42:42 +0000

    scaffolding training function

--- training.py
+++ training.py
@@ -1,4 +1,6 @@
-def train(model_class, dataset_name: str, Np: int, Nf: int, hidden_dim: int = 128, num_layers: int = 2, dropout: float = 0.1, process_function = None, batch_size: int = 32, epochs: int = 50, learning_rate: float = 1e-3, shuffle: bool = True, save_path: str = "model.pth"):
+from torch.utils.data import DataLoader
+
+def train(model, dataloaders: tuple[DataLoader, DataLoader], loss_function, optimizer, epochs: int = 50, learning_rate: float = 1e-3, save_path: str = "model.pth"):
     # Initialise model
     # Load dataset
     # Train model
